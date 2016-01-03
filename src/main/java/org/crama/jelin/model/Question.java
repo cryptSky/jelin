0f939493fd1b2fad8109ec65451a13817c917224
kryvol <vkrytskyy@gmail.com>
2016-01-03 23:35:12 +0200

    Added annotation to Question

--- src/main/java/org/crama/jelin/model/Question.java
+++ src/main/java/org/crama/jelin/model/Question.java
@@ -4,15 +4,19 @@ import java.io.Serializable;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@Entity
+@Table(name = "Question")
 public class Question implements Serializable {
 	
 	private static final long serialVersionUID = -4453888888834781L;
